c80a66c6ee33f6b042654afa6d4c120278eff999
KaranRangwani <karanrangwani1990@gmail.com>
2026-08-04 18:06:37 +0530

    updated program

--- Practice_Questions/question8.cpp
+++ Practice_Questions/question8.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 using namespace std ; 
 int main() { 
-    float pi = 3.14 ; 
-    int r ; 
-    cout<<"Enter radius of circle (in cm): \n" ; 
-    cin>>r ; 
-    int area = pi*r
+    float radius ;
+    float pi ;
+    cout<<"Enter radius of the circle (in cm) : \n" ; 
+
+    cin>>radius ;
+    float area = pi*radius*radius ;  
+    cout<<"The area of circle is "<<area<<" cm \n" ;
+    float diameter = 2*radius ; 
+    cout<<"The diameter of circle is "<<diameter<<" cm \n" ; 
+    float circumference = 2*pi*radius ; 
+    cout<<"The circumference of circle is "<<circumference<<" cm" ; 
+
     return 0 ; 
 
 }
