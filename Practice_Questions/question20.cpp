bbab34ef8cac2ff866e382308f4a3d2fd35d7ae9
Karan Rangwani <karanrangwani1990@gmail.com>
2026-08-09 13:19:18 +0530

    Question Practice

---
+++ Practice_Questions/question20.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main () { 
+
+int l = 12 ; 
+int b = 5 ; 
+cout<<"Whats the area of rectangle ? " <<endl ; 
+int area = l*b ; 
+cout<<"Area is "<<area<<endl ; 
+cout<<"Whats the perimeter of reactangle ?"<<endl ; 
+int perimeter = 2*(l+b) ; 
+cout<<"Perimeter is "<<perimeter<<endl ; 
+return 0 ; 
+
+
+
+}
