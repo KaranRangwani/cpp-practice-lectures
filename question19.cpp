a044da4b898114bb7d30a819ad5062234e350b3e
Karan Rangwani <karanrangwani1990@gmail.com>
2026-08-09 08:27:41 +0530

    Question practice

---
+++ question19.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ; 
+int main () { 
+    int a = 10 ; 
+    int b = 5 ; 
+    int c = 2 ; 
+    cout<<"The value of the following is ((a+b) * c - a/b) : "<<((a+b) * c - a/b)<<endl ;
+    return 0 ; 
+}
