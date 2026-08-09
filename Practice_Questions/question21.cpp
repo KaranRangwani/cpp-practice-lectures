d80242c273590614a0adb7a51de1614cec3edae7
Karan Rangwani <karanrangwani1990@gmail.com>
2026-08-09 13:21:40 +0530

    Coorection

--- Practice_Questions/question21.cpp
+++ Practice_Questions/question21.cpp
@@ -6,7 +6,7 @@ int main() {
     cout<<temprature<<" .C"<<endl ; 
     cout<<"To convert celcius into fahrenhiet,the formula is F = (C * 9/5) + 32"<<endl ; 
     float F = (temprature * 9/5) + 32 ; 
-    cout<<"The temprature in fahrenhite is : "<<F<<".F"<<endl ; 
+    cout<<"The temprature in fahrenhiet is : "<<F<<".F"<<endl ; 
     return 0; 
 
 }
